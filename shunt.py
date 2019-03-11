bfa2a66af47f3cde69b6c84dede666884d70eadf
aaronBurns59 <aaronburns59@gmail.com>
2019-03-11 11:52:55 +0000

    NOT WORKING problem with 'string out of index range'

--- shunt.py
+++ shunt.py
@@ -4,12 +4,12 @@
 def shunt(infix):
     #A dictionary allows setting a higher order of precidences for its items
     #dictionaries are order by strings precidence not an index like an array
-    specialSymbols= {'*':50, '.': 40, '|': 30}
+    specials= {'*':50, '.': 40, '|': 30}
 
-    #push and pop operators to this string (push from infix - pop to postfix)
-    stack=""
     #return after the infix has been changed to postfix
     postfix=""
+    #push and pop operators to this string (push from infix - pop to postfix)
+    stack=""
 
     for c in infix:
         if c == '(':
@@ -17,17 +17,25 @@ def shunt(infix):
         elif c == ')':
             #[-1] means the last element in the stack
             while stack[-1] != '(':
-                postfix = postfix + stack[-1]
+                postfix, stack = postfix + stack[-1], stack[:-1]
                 #sets the stack equal to upto the last character but not including
                 #removes last character from stack
-                stack= stack[:-1]
             #run again to delete the closing bracket
             stack= stack[:-1]
         #is the character in the dictionary
-        elif c in specialSymbols:
-
+        elif c in specials:
+        #once you've read a special character
+            while stack and specials.get(c, 0) <= specials.get(stack[-1],0):
+                postfix, stack = postfix + stack[-1], stack[:-1]
+            #puts the special character on the stack
+            stack = stack + c
         else:
             postfix = postfix + c
-
-
+        #anything thats at the end of the stack is added to the postfix and removed from the stack
+        while stack:
+            #postfix = postfix + stack[-1]
+            #stack= stack[:-1]
+            #can do all in one line
+            postfix, stack = postfix + stack[-1], stack[:-1]
     return postfix
+#print(shunt("(a.b)|(c*.d)"))
